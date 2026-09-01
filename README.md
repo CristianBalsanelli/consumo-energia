113c8d77a5e77f27273277d3a5a69a1d391f5a26
CristianBalsanelli <cristianbalsanelli@gmail.com>
2026-09-01 19:21:01 -0300

    programa de cálculo de consumo elétrico e valor mensal médio gasto

---
+++ README.md
@@ -0,0 +1,7 @@
+Calculadora de consumo de aparelhos elétricos
+Linguagem: Python
+Fórmula usada para cálculo: 
+custoKW = 0.75
+consumoMensal = (potencia * horasDia * 30)/1000
+valorMensal = consumoMensal * custoKW
+Instruções para executar o programa: Rode o programa, digite o nome de um aparelho elétrico, digite a sua potência em watts, digite o tempo utilizado em horas por dia, na variavel custoKW informa o valor do KW cobrado pela concessionária de energia elétrica
---
+++ app.py
@@ -0,0 +1,14 @@
+# Programa de cálculo de consumo elétrico e valor mensal médio gasto 
+# # Autor: Cristian
+# Entrada 
+nome = input("Digite o nome do aparelho elétrico: ") 
+potencia = float(input("Digite a sua potência em watts: ")) 
+horasDia = float(input("Digite o tempo médio de uso diário em horas: ")) 
+# Processamento 
+custoKW = 0.75
+consumoMensal = (potencia * horasDia * 30)/1000
+valorMensal = consumoMensal * custoKW
+# Saída 
+print(f"\nAparelho: {nome}") 
+print(f"Consumo estimado: {consumoMensal:.2f}Kw") 
+print(f"Valor mensal médio gasto: R${valorMensal:.2f}")
